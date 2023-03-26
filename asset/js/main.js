35d315d21d610bb4412eca17bd951d9a3c59ba45
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-27 00:38:53 +0200

    FASE 1 - creo primo ciclo per stampare 100 numeri

--- asset/js/main.js
+++ asset/js/main.js
@@ -46,3 +46,16 @@ Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
+
+
+let ul = document.querySelector('ul');
+
+
+for (let i = 1; i <= 100; i++) {
+    ul.innerHTML += 
+    `
+    <li>
+        ${i}
+    </li>
+    `
+}
